955a5b7fea94f3e7416455ddf979a330239de731
cmota <cafonsomota@gmail.com>
2021-06-14 16:27:47 +0100

    Removed unused import

--- 07-read-the-keyboard-visibility/Starter/app/src/main/java/com/raywenderlich/android/braindump/ui/Chat.kt
+++ 07-read-the-keyboard-visibility/Starter/app/src/main/java/com/raywenderlich/android/braindump/ui/Chat.kt
@@ -49,7 +49,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
